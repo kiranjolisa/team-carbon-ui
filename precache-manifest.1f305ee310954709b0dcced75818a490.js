b8397b3cc64fece3509ba263e241abd7a45191b7
Kiran <kiranjolisa@gmail.com>
2021-04-19 16:24:26 +0530

    Updates

---
+++ precache-manifest.1f305ee310954709b0dcced75818a490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89db1d7a525b03e3521315cf0257ec56",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1aab89f1e5097c8ee0be",
+    "url": "./static/css/main.110ca62f.chunk.css"
+  },
+  {
+    "revision": "921b2191544f91094d9a",
+    "url": "./static/js/2.dba61c26.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.dba61c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aab89f1e5097c8ee0be",
+    "url": "./static/js/main.9d9e167c.chunk.js"
+  },
+  {
+    "revision": "d150df48ca2839deb194",
+    "url": "./static/js/runtime-main.24eaf250.js"
+  }
+]);
